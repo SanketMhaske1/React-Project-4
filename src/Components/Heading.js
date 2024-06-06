4ce1165c138208a89c95ed1aaeb5adce9df71688
SanketMhaske1 <sanketmhaske005@gmail.com>
2024-06-06 19:33:32 +0530

    React Project 4

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Heading = () => {
   return (
     <div className="flex flex-col gap-2 mb-4 justify-center">
-      {/* <h1 className="text-4xl font-bold ">Our Testimonials</h1> */}
+      <h1 className="text-4xl font-bold ">Our Testimonials</h1>
       <div
         className="h-1 bg-fuchsia-600 my-3 mx-auto"
         style={{ width: "200px" }}
